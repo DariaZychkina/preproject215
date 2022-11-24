91f45a0cca61838b90e1d79e13a7f9eb6531620c
DariaZychkina <dariazychkina@gmail.com>
2022-11-24 10:36:53 +0300

    comm

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,24 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+
+@Component
+public class Deth8 {
+    @Override
+    public String toString() {
+
+        return """
+                Всё!
+                Не боится ужасный Кощей
+                Ни воды, ни огня, ни мечей!
+                Но, вчера посмотрев телевизор,
+                Стал бояться он «Киндер-сюрпризов»!
+                """;
+    }
+}
